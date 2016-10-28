78f97dd9946c2b580a86a220ca6b486e07e8459a
hl.he <1198432354@qq.com>
2016-10-28 17:20:34 +0800

    update buildAndroid.sh

--- buildAndroid.sh
+++ buildAndroid.sh
@@ -11,21 +11,38 @@ PREFIX=$(pwd)/android/aarch64
 #export LD=aarch64-linux-android-ld
 #export AR=aarch64-linux-android-ar
 
+#./configure --list-decoders
 
 ./configure --prefix=$PREFIX \
 	--enable-shared \
 	--disable-static \
 	--enable-cross-compile \
 	--cross-prefix=aarch64-linux-android- \
-	--target-os=android \
-	--disable-avfilter \
-	--disable-encoders \
-	--disable-avdevice \
-	--disable-doc \
-	--disable-muxers \
-	--disable-filters \
-	--disable-devices \
 	--arch=aarch64 \
+	--target-os=android \
+	--disable-decoders \
+	--enable-decoder=h264 \
+	--enable-decoder=aac \
+	--disable-encoders \
+	--disable-muxers \
+	--disable-demuxers \
+	--enable-demuxer=aac \
+	--enable-demuxer=h264 \
+	--enable-demuxer=mpegvideo \
+	--enable-demuxer=ogg \
+	--enable-demuxer=m4v \
+	--disable-avfilter \
+	--disable-parsers \
+	--enable-parser=aac \
+	--enable-parser=aac_latm \
+	--enable-parser=h264 \
+	--disable-protocols \
+	--enable-protocol=file \
+	--disable-bsfs \
+	--disable-hwaccels \
+	--disable-avdevice \
+	--disable-devices \
+	--disable-doc \
 	--extra-cflags="-O3 -Wall -fpic" 
 #	--extra-ldflags=""
 
