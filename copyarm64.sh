742fa0078ab4b8459059e39adbe6f871016ededa
hanloong ho <1198432354@qq.com>
2016-11-25 17:04:20 +0800

    添加拷贝脚本

---
+++ copyarm64.sh
@@ -0,0 +1,1 @@
+ cp android/armv8-a/lib/*.so /media/sf_E_DRIVE/EclipseSource/demo_ffmpeg/jni/prebuilt/arm64-v8a/
---
+++ copyarm7a.sh
@@ -0,0 +1,2 @@
+ cp android/armv7-a/lib/*.so /media/sf_E_DRIVE/EclipseSource/demo_ffmpeg/jni/prebuilt/armeabi-v7a/
+
