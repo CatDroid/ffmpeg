e6219def0ea21752a645c93ea23a366916869f60
hanlon <1198432354@qq.com>
2017-01-26 15:30:30 +0800

    增加libopencore-amr opus支持

--- buildubuntu.sh
+++ buildubuntu.sh
@@ -2,30 +2,44 @@
 
 # 查看 https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu 获得更多编译脚本写法
 # CompilationGuide 把所有的外部库都放在目录 $HOME/ffmpeg_build 下 lib/ include/  lib/pkgconfig 
+# 这样就只需要:
+# --extra-cflags="-I$HOME/ffmpeg_build/include" \
+# --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+
+#libx264 需要 enable-gpl
+#opencore-amr 需要 --enable-version3    GPLv3
 
 #./configure --list-decoders 查看支持的decoders
+
+# PKG_CONFIG_PATH 多个路径之间需要用冒号 : 来分开
 
 PREFIX=$(pwd)/ubuntu
 
 X264_PATH=/home/hanlon/Cat6/git_qq_yun/x264/ubuntu/
+AMR_PATH=/home/hanlon/Cat6/git_qq_yun/opencore-amr-0.1.4/ubuntu
+OPUS_PATH=/home/hanlon/Cat6/git_qq_yun/opus/ubuntu
 
+MY_CFLAGS="-I${X264_PATH}/include -I${AMR_PATH}/include -I${OPUS_PATH}/include"
+MY_LDFLAGS="-L${X264_PATH}/lib -L${AMR_PATH}/lib -L${OPUS_PATH}/lib"
+MY_CONFIG_PATH="${X264_PATH}/lib/pkgconfig:${AMR_PATH}/lib/pkgconfig:${OPUS_PATH}/lib/pkgconfig"
 
-PKG_CONFIG_PATH="${X264_PATH}/lib/pkgconfig" ./configure --prefix=$PREFIX \
+PKG_CONFIG_PATH=${MY_CONFIG_PATH} ./configure --prefix=$PREFIX \
 	--enable-shared \
 	--disable-static \
 	--disable-doc \
 	--target-os=linux\
 	--enable-rpath \
-	--extra-cflags="-Os -Wall -fpic -I${X264_PATH}/include" \
-	--extra-ldflags="-L${X264_PATH}/lib" \
+	--extra-cflags="-Os -Wall -fpic ${MY_CFLAGS}" \
+	--extra-ldflags="${MY_LDFLAGS}" \
 	--enable-sdl \
 	--enable-ffprobe \
  	--enable-ffplay \
  	--enable-libx264 \
-	--enable-gpl 
-	 
-	
-
+	--enable-gpl \
+	--enable-libopencore-amrnb \
+	--enable-libopencore-amrwb \
+	--enable-version3 \
+	--enable-libopus
 
 #	--disable-decoders \
 #	--enable-decoder=h264 \
